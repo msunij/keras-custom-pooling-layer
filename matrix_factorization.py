5828e287a9347f8947bdc5e2d919f0cf570c80fe
msunij <msunij1993@gmail.com>
2019-10-25 16:52:29 +0530

    matrix factorization

---
+++ matrix_factorization.py
@@ -0,0 +1,35 @@
+import numpy as np 
+
+def factorization(R, P, Q, K, steps=5000, alpha=0.0002, beta=0.02):
+    Q = Q.T
+    for step in range(steps):
+        for i in range(len(R)):
+            for j in range(len(R[i])):
+                if R[i][j] > 0:
+                    eij = R[i][j] - np.dot(P[i,:],Q[:,j])
+                    for k in range(K):
+                        P[i][k] = P[i][k] + alpha * (2 * eij * Q[k][j] - beta * P[i][k])
+                        Q[k][j] = Q[k][j] + alpha * (2 * eij * P[i][k] - beta * Q[k][j])
+        eR = np.dot(P,Q)
+        e = 0
+        for i in range(len(R)):
+            for j in range(len(R[i])):
+                if R[i][j] > 0:
+                    e = e + pow(R[i][j] - np.dot(P[i,:],Q[:,j]), 2)
+                    for k in range(K):
+                        e = e + (beta/2) * (pow(P[i][k],2) + pow(Q[k][j],2))
+        if e < 0.001:
+            break
+    return P, Q.T
+
+def matrix_factorization(matrix,number_of_features=2):
+    row_count = len(matrix)
+    col_count = len(matrix[0])
+
+    P = np.random.rand(row_count,number_of_features)
+    Q = np.random.rand(col_count,number_of_features)
+
+    nP,nQ = factorization(matrix,P,Q,number_of_features)
+
+    return np.dot(nP,nQ.T)
+
